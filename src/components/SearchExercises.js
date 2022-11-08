f2c30049cb42191706a2618ea0ec3e508aaec7f6
gukasyan-arman <gukasian.a.a@gmail.com>
2022-11-08 23:01:33 +0400

    SearchComponent & adaptive

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import {Box, Button, Stack, TextField, Typography} from '@mui/material'
+import classes from "./styles/SearchExercises.module.css"
 
 function SearchExercises() {
+
+	const [search, setSearch] = useState('')
+
+	const handleSearch = async () => {
+		if(search.trim()) {
+			// const exercisesData = await fetchData();
+		}
+	}
+
 	return ( 
-		<div>
-			
-		</div>
+		<Stack className={classes.container}>
+			<Typography className={classes.title}>
+				Awesome Exercise You <br/> Should Know
+			</Typography>
+			<Box className={classes.inputWrapper}>
+				<TextField 
+					value={search}
+					onChange={(e) => setSearch(e.target.value.toLowerCase())}
+					label="Search Exercises" type="search"
+					className={classes.searchInput}
+				/>
+				<Button 
+					href="#search" variant='contained' className={classes.redBtn}
+					onClick={handleSearch}
+				>
+					Search
+				</Button>
+			</Box>
+		</Stack>
 	);
 }
 
