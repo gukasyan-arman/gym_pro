b928cb1b26120f73a18554e4b8d73077d7e9d015
gukasyan-arman <gukasian.a.a@gmail.com>
2022-11-08 23:52:06 +0400

    Fetch data from search

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,14 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import {Box, Button, Stack, TextField, Typography} from '@mui/material'
 import classes from "./styles/SearchExercises.module.css"
+import {exerciseOptions, fetchData} from '../utils/fetchData.js'
 
 function SearchExercises() {
 
 	const [search, setSearch] = useState('')
+	const [bodyParts, setBodyParts] = useState([])
+	const [exercises, setExercises] = ([])
+
+	useEffect(() => {
+        const fetchExercisesData = async () => {
+			const bodyPartsData = await fetchData(
+					'https://exercisedb.p.rapidapi.com/exercises/bodyPartList/bodyPartList',
+					exerciseOptions
+				);
+			setBodyParts(['all', ...bodyPartsData]);
+		}
+
+		fetchExercisesData();
+
+	}, [])
 
 	const handleSearch = async () => {
 		if(search.trim()) {
-			// const exercisesData = await fetchData();
+			const exercisesData = await fetchData(
+				'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+				 exerciseOptions
+			);
+
+			const searchedExercises = exercisesData.filter(
+				(exercise) => {
+					exercise.name.toLowerCase().includes(search) ||
+					exercise.target.toLowerCase().includes(search) ||
+					exercise.equipment.toLowerCase().includes(search) ||
+					exercise.bodyPart.toLowerCase().includes(search)
+				}
+			)
+
+			setSearch('');
+			setExercises(searchedExercises);
 		}
 	}
 
