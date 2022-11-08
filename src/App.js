5c97782bee5593d2140e5622b09db3d9449c0a78
gukasyan-arman <gukasian.a.a@gmail.com>
2022-11-08 17:00:23 +0400

    NavBar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import ExerciseDetail from './pages/ExerciseDetail';
+import './App.css'
 
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+import Logo from '../assets/images/Logo.png';
+import classes from "./styles/NavBar.module.css"
 
 function Navbar() {
 	return (
-		<div>Navbar</div>
-
+		<Stack 
+			direction='row' justifyContent='space-around'
+			sx={{gap:{sm: '122px', xs: '40px'}, mt: {sm: '32px', xs: '20px'}, justifyContent: 'none'}} px='20px'>
+			<Link to="/">
+				<img className={classes.logo} src={Logo} alt='logo' />
+			</Link>
+			<Stack 
+				direction='row' gap='40px' fontSize='24px' alignItems='flex-end'
+				sx={{marginRight:{xs: '10px'}, fontSize: {xs: '18px'}}}
+			>
+				<Link to="/" className={classes.link}>Home</Link>
+				<a href="#exercises" className={classes.link}>Exercises</a>
+			</Stack>
+		</Stack>
 	);
 }
 
